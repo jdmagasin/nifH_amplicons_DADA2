d3a12284459b7151754a48e52c07522a2ba9d8a1
Jonathan Magasin <jmagasin@ucsc.edu>
2023-07-19 14:16:02 -0700

    Improve error messages.

--- scripts.ancillary/Annotation/BlastnARB2017/blastnARB2017.sh
+++ scripts.ancillary/Annotation/BlastnARB2017/blastnARB2017.sh
@@ -11,14 +11,14 @@ USAGE="blastnARB2017.sh nuclFasta outPath [queryPctId] [queryPctCvg]"
 
 fasta=$1
 if [ ! -f "$fasta" ] ; then
-    echo "No fasta!"
-    echo "$USAGE"
+    echo "You did not specify a nucleotide FASTA file (nuclFasta)."
+    echo -e "Usage:\n\t$USAGE\n"
     exit -1
 fi
 ODIR=$2
-if [ -d "$ODIR" ] ; then
-    echo "Output directory $ODIR already exists."
-    echo "$USAGE"
+if [ -z "$ODIR" ] || [ -d "$ODIR" ] ; then
+    echo "Please specify an output directory (outPath) that does not already exist."
+    echo -e "Usage:\n\t$USAGE\n"
     exit -1
 fi
 pid=98

1ec2740d4d0f933a08c011fc80239767d18f4fa1
Jonathan Magasin <jmagasin@ucsc.edu>
2026-03-26 18:27:15 -0700

    New option -merge to create a single AUID abundance table rather than the separate 'count.*' tables.

--- scripts.ancillary/ASVs_to_AUIDs/makeAUIDCountTable.R
+++ scripts.ancillary/ASVs_to_AUIDs/makeAUIDCountTable.R
@@ -3,9 +3,9 @@
 ## Copyright (C) 2023 Jonathan D. Magasin
 
 ##
-## Create AUID count tables by returning to the ASV count tables and
-## substituting in the AUID for the ASV.  The ASV-to-AUID mapping is within the
-## table assignAUIDs2ASVs.R. The table also includes a Source column for the ASV
+## For each ASV count table create an AUID count table by substituting in the
+## AUID for the ASV id.  The ASV-to-AUID mapping is within the table returned by
+## assignAUIDs2ASVs.R. The table also includes a Source column for the ASV
 ## FASTA, which should be alongside the ASV count table.  So we should have
 ## enough to make the relabled tables.
 ##
@@ -29,8 +29,9 @@ should uniquely describe the DADA2 run that produced the ASV table,
 e.g. 'Arctic2016:DNA:Filt0.2'
 
 Options:
-    -md5  Relabel ASVs with their md5 checksums, not their AUIDs.
-
+    -md5    Relabel ASVs with their md5 checksums, not their AUIDs.
+    -merge  Rather than separate files, produce one table with all abundances,
+            AUID_abundances.tsv.
 "
 
 args <- commandArgs(trailingOnly=T)
@@ -39,7 +40,8 @@ if (length(auidInfoTsv) == 0 || !file.exists(auidInfoTsv)) {
     cat(usageStr)
     quit(save='no')
 }
-useMd5 <- (length(args) >1 && args[2]=='-md5')
+useMd5 <- ('-md5' %in% args)
+mergeTabs <- ('-merge' %in% args)
 
 
 auidTab <- read.table(auidInfoTsv, header=T, sep='\t')[,c('Source','ASVid','AUID','md5','Tag','Length')]
@@ -68,8 +70,8 @@ MapOneAsvTable <- function(tsv, tag, useMd5=F, verbose=T)
     idx <- match(rownames(asvTab), auidSS$ASVid)            # Which ASVs have AUIDs
     if (verbose) {
         cat("The ASV table", tsv, "has tag", tag, "and contains", nrow(asvTab), "ASVs. ",
-	    sum(!is.na(idx)), "of the ASVs were previously mapped by assignAUIDs2ASVs.R to AUIDs",
-	        "and will be included in the new AUID count table.\n")
+            sum(!is.na(idx)), "of the ASVs were previously mapped by assignAUIDs2ASVs.R to AUIDs",
+                "and will be included in the new AUID count table.\n")
     }
     asvTab <- asvTab[!is.na(idx),,drop=F]                  # Throw out unmapped ASVs
     newLab <- c('AUID','md5')[useMd5+1]
@@ -77,13 +79,51 @@ MapOneAsvTable <- function(tsv, tag, useMd5=F, verbose=T)
     asvTab
 }
 
+allTabs <- list()
 for (tag in names(asvTsvs)) {
     ## Make a valid R name, which should also be a valid file name. At least
     ## slashes and other wierd characters will be turned to periods.
-    outfile <- paste0('counts.',make.names(tag),'.tsv')
-    cat("### Working on",outfile,"###\n")
-    if (file.exists(outfile)) { stop(outfile," already exits!") }
+    if (!mergeTabs) {
+        outfile <- paste0('counts.',make.names(tag),'.tsv')
+        cat("### Working on",outfile,"###\n")
+        if (file.exists(outfile)) { stop(outfile," already exits!") }
+    } else {
+        cat("### Working on",tag,"###\n")
+    }
     newTab <- MapOneAsvTable(asvTsvs[tag], tag, useMd5)
-    write.table(newTab, file=outfile, sep='\t', quote=F)
+    if (!mergeTabs) {
+        write.table(newTab, file=outfile, sep='\t', quote=F)
+    } else {
+        allTabs[[tag]] <- newTab
+    }
     cat("\n")
+}
+
+if (mergeTabs) {
+    outfile <- 'AUID_abundances.tsv'
+    cat("Merging", length(allTabs), "AUID abundance tables into a single table,", outfile, "...")
+    x <- lapply(allTabs, function (tab) {
+        tab$label <- rownames(tab)  # The rownames could be AUIDs or md5s
+        rownames(tab) <- NULL
+        tab
+    })
+    ## Make sure colnames are distinct in each table.  The only exception is label.
+    stopifnot(names(which(table(unlist(sapply(x, colnames))) != 1)) == 'label')
+    m <- x[[1]]
+    if (length(x) > 1) {
+        for (i in 2:length(x)) {
+            m <- merge(m, x[[i]], by='label', all=T)
+        }
+        ## merge() used NA when an AUID was absent in a table.
+        m[is.na(m)] <- 0
+    }
+    ## Verify ASV abundance preserved.
+    stopifnot(sum(sapply(allTabs, function(x) sum(x[,colnames(x)!='label']))) == sum(m[,-1]))
+    ## For consistency with the separate count tables, make the row names be the AUIDs (or md5s)
+    stopifnot(length(unique(m$label)) == nrow(m))
+    rownames(m) <- m$label
+    m <- m[,colnames(m) != 'label']
+    m <- m[order(rowSums(m), decreasing=T),]
+    cat("There are", nrow(m), "AUIDs and", ncol(m), "samples.\n")
+    write.table(m, file="AUID_abundances.tsv", sep="\t", row.names=T, quote=F)
 }
